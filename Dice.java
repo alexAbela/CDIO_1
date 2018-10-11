0826c416cf3c3d97a198e71c766ae084222f51e1
Martin <martingw@live.dk>
2018-10-11 14:32:01 +0200

    Test for tilfældighed
    Test for tilfældighed af terningen + dice classen

---
+++ Dice.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Dice{
+    Random random = new Random();
+    private final int maxValue = 6;
+    private int value;
+    public Dice() {
+        value = 1;
+    }
+    public int roll(){
+        value = random.nextInt(maxValue) + 1;
+        return value;
+    }
+    public int getValue(){
+        return value;
+    }
+}
